19cdc8fde761749a28f2ca62e005e87bd9d4b59f
Ziadov37 <77000963+Ziadov37@users.noreply.github.com>
2022-04-12 12:10:43 +0000

    Repas schema

---
+++ server/model/Repas.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const RepasSchema = new mongoose.Schema({
+    name : {
+        type:String,
+        required:true,
+        min:4
+    },
+    description : {
+        type:String,
+        required:true,
+        max : 255
+    },
+    category : {
+        type:String,
+        required:true,
+        min : 4 
+    },
+    images : [{
+        type: String,
+        required: true,
+    }],
+    restaurant: [{ 
+        type: Schema.Types.ObjectId, 
+        ref: "Restaurant" 
+    }],
+});
+
+module.exports = mongoose.model('Repas', RepasSchema);
