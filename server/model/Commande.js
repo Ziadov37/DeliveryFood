1c25ac62d09db097a03efd906d272222268ff3ab
Ziadov37 <77000963+Ziadov37@users.noreply.github.com>
2022-04-13 23:53:47 +0000

    Commande model

--- server/model/Commande.js
+++ server/model/Commande.js
@@ -1,24 +1,54 @@
 const mongoose = require('mongoose');
 
 const CommandeSchema = new mongoose.Schema({
+    orderItems: [
+        {
+          qty: { 
+            type: Number, 
+            required: true 
+          },
+          repasId: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: 'Repas',
+          },
+        },
+      ],
     status : {
         type: String,
         required: true,
         default: "Waiting",
         enum:["Waiting","Delivered"]
-    },
+      },
     user: [{ 
         type: mongoose.Schema.Types.ObjectId, 
+        required: true,
         ref: "User" 
-    }],
-    restaurant: [{ 
-        type: mongoose.Schema.Types.ObjectId, 
-        ref: "Restaurant" 
-    }],
-    repas: [{ 
-        type: mongoose.Schema.Types.ObjectId, 
-        ref: "Repas" 
-    }],
+      }],
+    shippingAddress: {
+        street: { type: String, required: true },
+        city: { type: String, required: true },
+      },
+    phoneNumber: {
+        type: String,
+        required: true,
+      },
+    totalPrice: {
+        type: Number,
+        required: true,
+        default: 0.0,
+      },
+    isDelivered: {
+        type: Boolean,
+        default: false,
+      },
+     deliveredAt: {
+        type: Date,
+      },
+    deliveredBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Livreur',
+      },
 });
 
 module.exports = mongoose.model('Commande', CommandeSchema);
